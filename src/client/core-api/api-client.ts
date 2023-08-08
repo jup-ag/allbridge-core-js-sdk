7db27100673774f670dc9e7d9dd6f1cfe09e39d4
Josh Teng <tengjosh+github@gmail.com>
2023-08-08 16:51:45 +0800

    switch to fetch

--- src/client/core-api/api-client.ts
+++ src/client/core-api/api-client.ts
@@ -1,4 +1,3 @@
-import axios, { Axios } from "axios";
 import { ChainSymbol } from "../../chains";
 import { ChainDetailsMap, PoolInfoMap, PoolKeyObject } from "../../tokens-info";
 import { VERSION } from "../../version";
@@ -29,21 +28,36 @@ export interface ApiClient {
 }
 
 export class ApiClientImpl implements ApiClient {
-  private api: Axios;
+  private baseUrl: string;
+  private headers: Record<string, string>;
 
   constructor(params: AllbridgeCoreClientParams) {
-    this.api = axios.create({
-      baseURL: params.coreApiUrl,
+    this.baseUrl = params.coreApiUrl;
+    this.headers = {
+      Accept: "application/json",
+      ...params.coreApiHeaders,
+      "x-Sdk-Agent": "AllbridgeCoreSDK/" + VERSION,
+    };
+  }
+
+  async fetchJson(url: string, options: RequestInit = {}): Promise<any> {
+    const response = await fetch(url, {
+      ...options,
       headers: {
-        Accept: "application/json",
-        ...params.coreApiHeaders,
-        "x-Sdk-Agent": "AllbridgeCoreSDK/" + VERSION,
+        ...this.headers,
+        ...options.headers,
       },
     });
+
+    if (!response.ok) {
+      throw new Error(`HTTP error! Status: ${response.status}`);
+    }
+
+    return response.json();
   }
 
   async getTokenInfo(): Promise<TokenInfo> {
-    const { data } = await this.api.get<ChainDetailsResponse>("/token-info");
+    const data = await this.fetchJson(`${this.baseUrl}/token-info`);
     return {
       chainDetailsMap: mapChainDetailsResponseToChainDetailsMap(data),
       poolInfoMap: mapChainDetailsResponseToPoolInfoMap(data),
@@ -51,15 +65,16 @@ export class ApiClientImpl implements ApiClient {
   }
 
   async getTransferStatus(chainSymbol: ChainSymbol, txId: string): Promise<TransferStatusResponse> {
-    const { data } = await this.api.get<TransferStatusResponse>(`/chain/${chainSymbol}/${txId}`);
-    return data;
+    return this.fetchJson(`${this.baseUrl}/chain/${chainSymbol}/${txId}`);
   }
 
   async getReceiveTransactionCost(args: ReceiveTransactionCostRequest): Promise<ReceiveTransactionCostResponse> {
-    const { data } = await this.api.post<ReceiveTransactionCostResponse>("/receive-fee", args, {
+    const data = await this.fetchJson(`${this.baseUrl}/receive-fee`, {
+      method: "POST",
       headers: {
         "Content-Type": "application/json",
       },
+      body: JSON.stringify(args),
     });
     return {
       exchangeRate: data.exchangeRate,
@@ -70,15 +85,13 @@ export class ApiClientImpl implements ApiClient {
 
   async getPoolInfoMap(pools: PoolKeyObject[] | PoolKeyObject): Promise<PoolInfoMap> {
     const poolKeys = pools instanceof Array ? pools : [pools];
-    const { data } = await this.api.post<PoolInfoResponse>(
-      "/pool-info",
-      { pools: poolKeys },
-      {
-        headers: {
-          "Content-Type": "application/json",
-        },
-      }
-    );
+    const data = await this.fetchJson(`${this.baseUrl}/pool-info`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ pools: poolKeys }),
+    });
     return mapPoolInfoResponseToPoolInfoMap(data);
   }
 }
